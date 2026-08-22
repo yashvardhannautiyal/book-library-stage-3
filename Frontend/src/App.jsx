e95b86492e2ac0bdfe4e6d101ed3720f0f4f84d7
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-08-22 19:36:05 +0530

    updated handling finished shelf

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -253,37 +253,16 @@ function App() {
     );
   };
 
-  //handle finished shelf
-  //marks a shelf as finished
+// marks exactly one shelf as the finished shelf
 const handleSetFinishedShelf = (shelfId) => {
-  // Update which shelf is the designated finished shelf
   setShelves((prevShelves) =>
     prevShelves.map((shelf) => ({
       ...shelf,
       isFinishedShelf: shelf.id === shelfId,
     })),
   );
-
-  // update books and keep - rating and finishedAt same for new finished shelf
-  setBooks((prevBooks) =>
-    prevBooks.map((book) => {
-      const isNowFinished = book.shelfId === shelfId;
-
-      if (isNowFinished) {
-        return {
-          ...book,
-          finishedAt: book.finishedAt ?? new Date().toISOString(),
-        };
-      }
-
-      return {
-        ...book,
-        rating: "",
-        finishedAt: null,
-      };
-    }),
-  );
 };
+
 
   //delete a shelf
 const handleDeleteShelf = (shelfId, destinationShelfId) => {
